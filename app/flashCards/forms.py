de58dc11f577c5c848cb9fced99e8db51cc38ef9
MichaelVanderputten <michael.vand3rputten@gmail.com>
2023-09-22 15:26:28 -0600

    Ver 0.3 UNFINISHED - added basic deck creation. untested

---
+++ app/flashCards/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, BooleanField, IntegerField, SubmitField
+from wtforms.validators import DataRequired
+
+class CreateDeckForm(FlaskForm):
+    name = StringField('Deck Name', validators=[DataRequired()])
+    creator = StringField('Creator Name', validators=[DataRequired()])
+    private = BooleanField('Private')
+    num_cards = IntegerField('Number of Cards', validators=[DataRequired()])
+    submit = SubmitField('Create Deck')
--- app/flashCards/views.py
+++ app/flashCards/views.py
@@ -1,6 +1,10 @@
 from flask import render_template, redirect, url_for, flash, request, current_app
 from flask_login import current_user, login_required
 
+from .forms import CreateDeckForm
+from .models import Deck, FlashCard
+from app import db
+
 from . import flash_cards_blueprint # blueprint
 
 @flash_cards_blueprint.route('/home')
@@ -12,4 +16,40 @@ def home():
 @login_required
 def createFC():
     return render_template('flashCards/createFC.html')
+
+@flash_cards_blueprint.route('/create_deck', methods=['GET', 'POST'])
+def create_deck():
+    form = CreateDeckForm()
+    if form.validate_on_submit():
+        new_deck = Deck(
+            name=form.name.data,
+            private=form.private.data,
+            creator_id=1  # Replace this later with logged-in user ID
+        )
+        db.session.add(new_deck)
+        db.session.flush()
+
+        num_cards = form.num_cards.data
+        flash_cards = []
+
+        for i in range(1, num_cards + 1):
+            question = request.form.get(f'question{i}')
+            answer = request.form.get(f'answer{i}')
+
+            if question and answer:
+                flash_card = FlashCard(
+                    question=question, 
+                    answer=answer, 
+                    deck_id=new_deck.id, 
+                    creator_id=1
+                )
+                flash_cards.append(flash_card)
+
+        db.session.add_all(flash_cards)
+        db.session.commit()
+
+        flash('New deck and flashcards created successfully.')
+        return redirect(url_for('index'))
+
+    return render_template('create_deck.html', form=form)
 
